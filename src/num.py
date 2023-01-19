afca2bffd4cd3ad0af26bca4778fe28fb7cb5fe0
Shaunak15 <patelshaunak37@gmail.com>
2023-01-19 13:40:53 -0500

    added in-code documentation

--- src/num.py
+++ src/num.py
@@ -1,6 +1,7 @@
-
+## Num class Summarizes a stram of numbers
 class Num:
-
+    
+    ## constructor created for Num class
     def __init__(self):
         self.n = 0
         self.mu = 0
@@ -9,7 +10,9 @@ class Num:
         self.lo = float('inf')
         self.hi = float('-inf')
 
-
+    ## add method adds the n value also,
+    ## It upadtes the values of lo,hi d, mu,m2 which is used for,
+    ## calculating standard devaiation. 
     def add(self, value: float):
         self.n = self.n + 1
         d = value - self.mu
@@ -18,10 +21,13 @@ class Num:
         self.lo = min(value, self.lo)
         self.hi = max(value, self.hi)
 
+    ## mid method return the mean. 
     def mid(self):
         return self.mu
 
-    def div(self): #return standard deviation using Welford's algorithm http://t.ly/nn_W
+    # div method uses Welford's algorithmn to calculate standard deviation,
+    # here is the link for Welford's algorithmn http://t.ly/nn_W
+    def div(self): 
         if((self.m2 < 0) or (self.n < 2)):
             return 0
         return pow((self.m2 / (self.n - 1)), 0.5)
--- src/sym.py
+++ src/sym.py
@@ -1,15 +1,18 @@
 import math
 
+## It is Summarize of the stream of symbols
 class Sym:
 
+    ## created constructor for SYM class
     def __init__(self):
         self.n = 0
         self.has = {}
         self.most = 0
         self.mode = None
 
+    ## add function updates the counts for the values that has been seen so far
+    ## it doesn't return any value
     def add(self, x: chr):
-
         self.n = self.n + 1
         if x in self.has:
             self.has[x] = self.has[x] + 1
@@ -20,9 +23,11 @@ class Sym:
             self.most = self.has[x]
             self.mode = x
     
+    ## Mid method returns the mode (most frequent)
     def mid(self):
         return self.mode
 
+    ## div method returns the entropy 
     def div(self):
         e = 0
         for i in self.has:
